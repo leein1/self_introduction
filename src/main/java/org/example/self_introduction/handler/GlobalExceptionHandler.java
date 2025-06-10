9dce00660051f88b3b86a240d2640cfeb15df7be
leeinwon <inwon.private@gmail.com>
2025-06-10 19:36:45 +0900

    전역 예외 핸들러의 각 메서드 내용 중, 예외 응답 객체생성을 builder -> ErrorResponseDTO의 static 메서드 로 수정 / path 추출 메서드 따로 뽑음

--- src/main/java/org/example/self_introduction/handler/GlobalExceptionHandler.java
+++ src/main/java/org/example/self_introduction/handler/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package org.example.self_introduction.handler;
 
-import org.apache.coyote.Response;
 import org.example.self_introduction.dto.ErrorResponseDTO;
 import org.example.self_introduction.exception.FileNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -13,44 +12,30 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private String getProcessPath(WebRequest request){
+
+        return request.getDescription(false).replaceAll("uri=", "");
+    }
 
     @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
     public ResponseEntity<ErrorResponseDTO> handleBadRequest(RuntimeException ex, WebRequest request) {
 
-        ErrorResponseDTO errorResponseDTO = ErrorResponseDTO.builder()
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .message(ex.getMessage())
-                .path(request.getDescription(false).replace("uri=", ""))
-                .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDTO);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponseDTO.badRequest(ex.getMessage(), this.getProcessPath(request)));
     }
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<ErrorResponseDTO> handleInternalServerError(Exception ex, WebRequest request) {
 
-        ErrorResponseDTO errorResponseDTO = ErrorResponseDTO.builder()
-                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
-                .message(ex.getMessage())
-                .path(request.getDescription(false).replace("uri=", ""))
-                .build();
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseDTO);
+        return ResponseEntity.internalServerError()
+                .body(ErrorResponseDTO.internalError(ex.getMessage(), this.getProcessPath(request)));
     }
 
     @ExceptionHandler(value ={FileNotFoundException.class})
     public ResponseEntity<ErrorResponseDTO> handleFileNotFound(FileNotFoundException ex, WebRequest request) {
 
-        ErrorResponseDTO errorResponseDTO = ErrorResponseDTO.builder()
-                .statusCode(HttpStatus.NOT_FOUND.value())
-                .error("File Not Found")
-                .message(ex.getMessage())
-                .path(request.getDescription(false).replace("uri=", ""))
-                .build();
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDTO);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponseDTO.notFound(ex.getMessage(), this.getProcessPath(request)));
     }
 
 }
