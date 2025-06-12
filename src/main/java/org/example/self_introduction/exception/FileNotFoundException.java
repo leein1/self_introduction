3fac4beb572946f2aa203db84a87be425444d2d8
leeinwon <inwon.private@gmail.com>
2025-06-12 16:05:26 +0900

    no message

--- src/main/java/org/example/self_introduction/exception/FileNotFoundException.java
+++ src/main/java/org/example/self_introduction/exception/FileNotFoundException.java
@@ -2,8 +2,8 @@ package org.example.self_introduction.exception;
 
 public class FileNotFoundException extends RuntimeException {
 
-    public FileNotFoundException(String filename) {
+    public FileNotFoundException(String fileName) {
 
-        super(filename + " 파일을 찾을 수 없습니다.");
+        super(fileName + " 파일을 찾을 수 없습니다.");
     }
 }
